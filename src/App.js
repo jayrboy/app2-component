90a03eb2a3da8d5df9ee2a266f7dc602fb97b142
jayrboy <j.jayox_@hotmail.com>
2024-01-08 11:24:41 +0700

    Add Class Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { Header, Content, Footer } from './func-component'
+import Calendar from './class-component'
 
 function App() {
-  return [<Header />, <Content />, <Footer />]
+  return (
+    <>
+      <Header />
+      <p>
+        <center>
+          <Calendar />
+        </center>
+      </p>
+      <Content />
+      <Footer />
+    </>
+  )
 }
 
 export default App
