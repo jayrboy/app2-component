db113fe0caa4c930c265662e6ce0183e3aa99c22
jayrboy <j.jayox_@hotmail.com>
2024-01-19 14:27:08 +0700

    Add Refs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 // import { Header, Content, Footer } from './components/func-component'
 // import Calendar from './components/class-component'
 // import { Button } from './components/class-component'
-import { EvenData, EvenData2, Table } from './components/EvenData'
+// import { EvenData, EvenData2, Table } from './components/EvenData'
+import RefsFunc from './components/RefsFunc'
 
 function App() {
   return (
     <>
-      <EvenData />
-      <hr />
-      <EvenData2 />
-      <hr />
-      <Table />
+      <RefsFunc />
     </>
   )
 }
