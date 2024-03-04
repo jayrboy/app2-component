050a0544e7f29a64586e4b8bf470a8af08e8dadd
jayrboy <j.jayox_@hotmail.com>
2024-03-04 11:01:27 +0700

    Add FormSelectFile

--- src/checkbox/CheckBox.js
+++ src/checkbox/CheckBox.js
@@ -1,10 +1,12 @@
 import FormCheck from './FormCheck'
+import FormSelectFile from './FormSelectFile'
 
 const CheckBox = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <FormCheck />
       <hr />
+      <FormSelectFile />
     </div>
   )
 }
